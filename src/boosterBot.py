603a9de65f5131817f7d4ce39927badba17b2970
Paul-Ole Saalfeld <p.saalfeld@flohr-cie.de>
2024-04-07 01:59:50 +0200

    cleaned bot

--- src/boosterBot.py
+++ src/boosterBot.py
@@ -10,32 +10,9 @@ intents.message_content = True
 intents.members = True
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!!"), intents=intents, test_guilds=[269566282494377984])
 
-
-@bot.slash_command(description="Lists the help page of this bot")
-async def help(inter):
-    await inter.response.send_message("""```
-/help               Lists the help page of this bot
-/ping (@...)        Pings yourself or someone if you mentioned him```""") # TODO: Create Embedding later
-    
-@bot.slash_command(description="Pings yourself or someone if you mentioned him")
-async def ping(inter, user: disnake.User = None):
-    if user is None:
-        await inter.response.send_message(f"<@{inter.user.id}> Pong!")
-    else: 
-        await inter.response.send_message(f"<@{user.id}> Pong!")
-
-
 @bot.event
 async def on_ready():
     print(f"Bot is ready! Logged in as: {bot.user} (ID: {bot.user.id})")
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content == "cookie":
-        await message.channel.send(":cookie:")
 
 @bot.event
 async def on_member_update(before, after):
@@ -56,7 +33,6 @@ async def on_member_update(before, after):
                 users = await reaction.users().flatten()
                 if after in users:
                     await reaction.remove(after)
-
 
 if __name__ == "__main__":
     bot.run(os.getenv("BOT_TOKEN"))
