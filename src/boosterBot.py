f44494f51aff5e335282e9e5e7f95288e639580e
Paul-Ole Saalfeld <p.saalfeld@flohr-cie.de>
2024-04-07 01:50:20 +0200

    completed functionality

--- src/boosterBot.py
+++ src/boosterBot.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 intents = disnake.Intents.default()
 intents.message_content = True
+intents.members = True
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!!"), intents=intents, test_guilds=[269566282494377984])
 
 
@@ -35,7 +36,27 @@ async def on_message(message):
         return
     if message.content == "cookie":
         await message.channel.send(":cookie:")
-    
+
+@bot.event
+async def on_member_update(before, after):
+    if len(before.roles) is not len(after.roles):
+        rolename = os.getenv("WATCH_ROLE_NAME")
+        logChannelID = int(os.getenv("LOG_CHANNEL_ID"))
+        logChannel = bot.get_channel(logChannelID)
+        reactionChannelID = int(os.getenv("REACTION_CHANNEL_ID"))
+        reactionMessageID = int(os.getenv("REACTION_MESSAGE_ID"))
+        reactionMessage = await bot.get_channel(reactionChannelID).fetch_message(reactionMessageID)
+        if (rolename in str(after.roles)) and not (rolename in str(before.roles)):
+            print(f"User {after.name} gained role {rolename}")
+            await logChannel.send(f"<@{after.id}> hat angefangen den Server zu boosten.")
+        if (rolename in str(before.roles)) and not (rolename in str(after.roles)):
+            print(f"User {after.name} lost role {rolename}")
+            await logChannel.send(f"<@{after.id}> hat aufgehört den Server zu boosten.")
+            for reaction in reactionMessage.reactions:
+                users = await reaction.users().flatten()
+                if after in users:
+                    await reaction.remove(after)
+
 
 if __name__ == "__main__":
     bot.run(os.getenv("BOT_TOKEN"))
